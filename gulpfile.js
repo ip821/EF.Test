96460a334eea0054055c60ef3627f95730ab2209
igor <igor>
2016-03-21 10:08:36 +0200

    Added vs files.

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,9 @@ var gulp = require('gulp');
 var exec = require('child_process').exec;
 
 gulp.task("default", function() {
-    exec("dnu restore", function(err, stdout, stderr) {
+    exec("dnu build ./Host --quiet", function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
-
-        exec("dnu build ./Host --quiet", function(err, stdout, stderr) {
-            console.log(stdout);
-            console.log(stderr);
-            console.log("Done.");
-        });
+        console.log("Done.");
     });
 });
